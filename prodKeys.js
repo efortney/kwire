c6ba4c9ab7e36a25f8f34b01b401167157ad9e21
devOpElliot <efortney@missouriwestern.edu>
2020-07-23 09:50:29 -0500

    casing

--- prodKeys.js
+++ prodKeys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clientID: process.env.clientId,
+  clientID: process.env.clientID,
   clientSecret: process.env.clientSecret,
   password: process.env.password,
   uri: process.env.uri,
